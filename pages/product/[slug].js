df405806010f55c3bf3af3bf9659504db0b838a5
DohosMate <dohosmail@gmail.com>
2022-07-07 17:18:58 +0200

    fizetés kivételével kész

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,24 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
-const ProductDetails = ({products, product}) => {
-    const {image, name, details, price} = product;
+
+const ProductDetails = ({ products, product }) => {
+    const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
+
     return (
         <div>
             <div
                 className="product-detail-container">
                 <div>
                     <div className="image-container">
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])}
+                            className="product-detail-image" />
+                    </div>
+                    {<div className="small-images-container">
+                        {image?.map((item, i) => (
+                            <img
+                                src={urlFor(item)}
+                                className={i === index ?
+                                    'small-image selected-image' :
+                                    'small-image'}
+                                onMouseEnter={() => setIndex(i)}
+                            />
+                        ))}
+                    </div>}
+                </div>
+                <div className='product-detail-desc'>
+                    <h1>{name}</h1>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+                        </div>
+                        <p>(20)</p>
+                    </div>
+                    <h4>Részletek: </h4>
+                    <p>{details}</p>
+                    <p className='price'>{price}Ft</p>
+                    <div className="quantity">
+                        <h3>Mennyiség:</h3>
+                        <p className='quantity-desc'>
+                            <span className='minus' onClick={decQty}>
+                                <AiOutlineMinus />
+                            </span>
+                            <span className='num' onClick="">
+                                {qty}
+                            </span>
+                            <span className='plus' onClick={incQty}>
+                                <AiOutlinePlus />
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button type='button' className='add-to-cart' onClick= {()=> onAdd(product, qty)}>Vásárlás most</button>
+                        <button type='button' className='buy-now' onClick="">Hozzáadás a bevásárlókosárhoz</button>
                     </div>
                 </div>
-
             </div>
+            <div className="maylike-products-wrapper">
+                <h2>Kóstold meg ezeket is!</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id}
+                                product={item} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
 
-export const getStaticPaths = async() => {
+export const getStaticPaths = async () => {
     const query = `*[_type == "product"]{
         slug{
             current
@@ -32,13 +96,13 @@ export const getStaticPaths = async() => {
         }
     }));
 
-    return{
+    return {
         paths,
         fallback: 'blocking'
     }
 }
 
-export const getStaticProps = async ({params: {slug}}) => {
+export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
     const productsQuery = '*[_type == "product"]'
     const product = await client.fetch(query);
