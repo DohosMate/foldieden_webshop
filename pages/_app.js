da253a47361e9ab279afe81525139c85fad7c0f1
DohosMate <dohosmail@gmail.com>
2022-08-09 13:48:45 +0200

    meta tags in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,18 @@ import React from 'react'
 import '../styles/globals.css'
 import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
+
     <StateContext>
       <Layout>
         <Toaster />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </StateContext>
